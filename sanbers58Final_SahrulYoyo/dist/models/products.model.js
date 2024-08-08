0189eee9a78a965d6c96520da2fc64fe0881daad
Sahrul Yoyo <111233171+sahrulyo@users.noreply.github.com>
2024-08-09 02:29:01 +0700

    update package.json start server

--- sanbers58Final_SahrulYoyo/dist/models/products.model.js
+++ sanbers58Final_SahrulYoyo/dist/models/products.model.js
@@ -1,44 +1,8 @@
 "use strict";
-// import mongoose from "mongoose";
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-// const Schema = mongoose.Schema;
-// const ProductsSchema = new Schema(
-//   {
-//     name: {
-//       type: String,
-//       required: true,
-//     },
-//     description: {
-//       type: String,
-//       required: true,
-//     },
-//     images: {
-//       type: [String],
-//       required: true,
-//     },
-//     price: {
-//       type: Number,
-//       required: true,
-//     },
-//     qty: {
-//       type: Number,
-//       required: true,
-//     },
-//     categoryId: {
-//       type: mongoose.Schema.Types.ObjectId,
-//       ref: "Category",
-//     },
-//   },
-//   {
-//     timestamps: true,
-//   }
-// );
-// const ProductsModel = mongoose.model("Products", ProductsSchema);
-// export default ProductsModel;
-//kode diubah untuk menambahkan "model kategori"
 const mongoose_1 = __importDefault(require("mongoose"));
 const Schema = mongoose_1.default.Schema;
 const ProductsSchema = new Schema({
